195a485eeee9c088b0b3ba9c0131dd9cdd4b83fc
Michael Mutinda <mmike8449@gmail.com>
2024-04-04 01:26:14 -0400

    css more styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,11 @@ import { useAuth } from '../contexts/AuthContext';
 import { ShopContext } from '../contexts/shop-context';
 import { useContext } from 'react';
 
-
 function Header() {
     const { isAuthenticated, setIsAuthenticated } = useAuth();
     const { compareItems } = useContext(ShopContext);
     const compareCount = Object.values(compareItems).filter(val => val).length;
+
     const handleLogout = () => {
         setIsAuthenticated(false);
         // Perform any additional logout logic here
@@ -17,7 +17,7 @@ function Header() {
     return (
         <header className="header">
              <div className="logo-container">
-               <div>Laptop Bazaar</div>
+                <div>Laptop Bazaar</div>
                 <nav className="navbar">
                     <ul className="nav-list">
                         {/* <li className="nav-item"><Link to="/">Laptop Bazaar</Link></li> */}
@@ -31,7 +31,17 @@ function Header() {
                             <li className="nav-item"><Link to="/login">Login</Link></li>
                         )}
                         <li className="nav-item"><Link to="/contact">Contact</Link></li>
-                        <li className="nav-item"><Link to="/cart">Cart</Link></li>
+                        <li className="nav-item">
+                            <Link to="/cart">
+                                Cart
+                                {/* SVG cart icon */}
+                                <svg className="cart-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <circle cx="9" cy="21" r="1"></circle>
+                                    <circle cx="20" cy="21" r="1"></circle>
+                                    <path d="M1 1h4l.9 4.45m.6 2.55h12.5a.5.5 0 0 0 .5-.5V6H7"></path>
+                                </svg>
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
